d93044b8ddafb509a1c2e38a7ec19057ba9f185c
Catherine Leung <cathyatseneca>
2019-03-25 02:26:49 -0400

    a3 starter files

---
+++ a3.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class SpellCheck{
+
+public:
+	SpellCheck(string data[],int sz);
+	void addWord(const string& newWord);
+	bool lookup(const string& word) const;
+	int suggest(const string& partialWord, string suggestions[]) const;
+	~SpellCheck();	
+};
